941a30b33ac7d56bf936e5a9970a9029c23d5071
thoai016774110 <thoai016774110@gmail.com>
2025-09-18 00:14:13 +0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Chào mừng đến với Blog App 🚀</h1>
-      <p className="mt-2 text-gray-600">Đây là project Next.js đầu tiên của bạn.</p>
+      <h1 className="text-2xl font-bold">
+        Chào mừng em Nga đến với tình iu của minh thoại 🚀
+      </h1>
+      {/* <p className="mt-2 text-gray-600">
+        Đây là project Next.js đầu tiên của bạn.
+      </p> */}
+
+      <img
+        src="/anh.png" 
+        alt="Tình yêu"
+        className="mt-4 w-64 rounded-xl shadow-lg"
+      />
     </div>
   );
 }
